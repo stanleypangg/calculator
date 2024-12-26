c6061c8ef0d7a1caf58d698ba963664f85def2e0
stanleypangg <144388332+stanleypangg@users.noreply.github.com>
2024-12-26 18:16:14 -0500

    basic calculator layout added

---
+++ script.js
@@ -0,0 +1,14 @@
+let a, op, b;
+
+function assignNumberByID(target, id) {
+    switch (id) {
+        case ""
+    }
+}
+
+const container = document.querySelector(".container");
+container.addEventListener("click", (e) => {
+    let id = e.target.id;
+    if (op) assignNumberByID(b, id);
+    else assignNumberByID(a, id);
+});
