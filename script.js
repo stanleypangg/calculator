02b07bf51b2c41b80be8274bb96dd80e1ad2463b
stanleypangg <144388332+stanleypangg@users.noreply.github.com>
2024-12-26 18:28:35 -0500

    number buttons functional on display

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-let a, op, b;
-
-function assignNumberByID(target, id) {
+function convertID(id) {
     switch (id) {
-        case ""
+        case "zero": return 0;
+        case "one": return 1;
+        case "two": return 2;
+        case "three": return 3;
+        case "four": return 4;
+        case "five": return 5;
+        case "six": return 6;
+        case "seven": return 7;
+        case "eight": return 8;
+        case "nine": return 9;
     }
 }
 
-const container = document.querySelector(".container");
-container.addEventListener("click", (e) => {
+let a, b, op;
+a = b = "";
+
+const display = document.querySelector(".display");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
     let id = e.target.id;
-    if (op) assignNumberByID(b, id);
-    else assignNumberByID(a, id);
+    let number = convertID(id)
+    if (op) {
+        b += number;
+        display.textContent = `${a} ${op} ${b}`;
+    } else {
+        a += number;
+        display.textContent = a;
+    }
 });
